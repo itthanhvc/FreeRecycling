ce8ca4564233db1365d70fa719940bfbb6c65d9a
itthanhvc <chienxa_1986@yahoo.com.vn>
2017-02-07 17:44:45 -0600

    add UserLogin

---
+++ client-side/src/app/model/login.model.ts
@@ -0,0 +1,9 @@
+export class LoginInfo {
+    // firstName: string;
+    // lastName: string;
+    // email: string;
+    // token: string;
+    constructor(public firstName: string,public lastName: string, public email: string, public token: string  ){
+
+    }
+}
