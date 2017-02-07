1d263c8c69ba37c39efa9297c9e94c1c9b1c8f6d
itthanhvc <chienxa_1986@yahoo.com.vn>
2017-02-07 01:51:13 -0600

    adjust layout

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +10,7 @@ import { myRoutes } from './app.routes';
 import { StateComponent } from './state/state.component';
 import { MyDonationComponent } from './donation/my-donation.component';
 import { NearByComponent } from './near-by/near-by.component';
+import { CollapseDirective } from 'ng2-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,11 @@ import { NearByComponent } from './near-by/near-by.component';
     LoginComponent,
     StateComponent,
     MyDonationComponent,
-    NearByComponent
+    NearByComponent,
+    CollapseDirective
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
